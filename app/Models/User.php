8bcd916d6dfb66157919eba7874646e17076d011
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-02-16 11:19:33 +0800

    added request migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(UserInformation::class);
     }
+
+    public function requests()
+    {
+        return $this->hasMany(Request::class);
+    }
 }
