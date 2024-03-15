4284c6c8dc1d7270d47c775dcbc9023798946b44
Gabriel Jon Icawalo <67619600+GabJeungSook@users.noreply.github.com>
2024-03-15 16:12:13 +0800

    wip

--- resources/views/tables/columns/days-requested.blade.php
+++ resources/views/tables/columns/days-requested.blade.php
@@ -1,3 +1,9 @@
 <div>
-    {{ Carbon\Carbon::parse($getRecord()->created_at)->diffInDays() }}
+    <p class="font-mono ml-12">
+        {{ Carbon\Carbon::parse($getRecord()->created_at)
+            ->isToday() ?
+            Carbon\Carbon::parse($getRecord()->created_at)->diffForHumans() :
+            Carbon\Carbon::parse($getRecord()->created_at)->diffInDays() . ' Day(s)'
+        }}
+    </p>
 </div>
