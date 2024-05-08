fc36499657a11dc615bf1ab8e1886dd5e0d71f18
GabJeungSook <gicawalo@gmail.com>
2024-05-08 09:04:02 +0800

    denied

--- resources/views/livewire/admin/review-pending-request.blade.php
+++ resources/views/livewire/admin/review-pending-request.blade.php
@@ -107,7 +107,7 @@
                     <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{Carbon\Carbon::parse($record->denied_at)->format('F d, Y h:i A')}}</dd>
                 </div>
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-900">Approved By</dt>
+                    <dt class="text-sm font-medium text-gray-900">Denied By</dt>
                     <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ucwords($record->approvedBy->name)}}</dd>
                 </div>
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
