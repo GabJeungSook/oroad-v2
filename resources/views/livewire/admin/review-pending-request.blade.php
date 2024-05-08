c636c2973516f6a3f68b7a1a1413ff4aa38a59dd
GabJeungSook <gicawalo@gmail.com>
2024-05-08 09:04:39 +0800

    ss

--- resources/views/livewire/admin/review-pending-request.blade.php
+++ resources/views/livewire/admin/review-pending-request.blade.php
@@ -111,7 +111,7 @@
                     <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ucwords($record->approvedBy->name)}}</dd>
                 </div>
                 <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                    <dt class="text-sm font-medium text-gray-900">Remarks</dt>
+                    <dt class="text-sm font-medium text-gray-900">Reason</dt>
                     <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{{ucwords($record->remarks)}}</dd>
                 </div>
                 @endif
